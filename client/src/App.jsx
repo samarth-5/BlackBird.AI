0cfa051ec7342fa9a2fb82ecaccbbeef76f0de4e
Samarth Narayan <kayaniv321@gmail.com>
2024-05-24 01:05:19 +0530

    files created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import SignIn from './Pages/SignIn.jsx';
+import SignUp from './Pages/SignUp.jsx';
+import Chatbot from './Pages/Chatbot.jsx';
+import Prompt2Image from './Pages/Prompt2Image.jsx';
+import Header from './Components/Header.jsx';
+import ScrollToTop from './Components/ScrollToTop.jsx';
+import PrivateRoute from './Components/PrivateRoute.jsx';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/' element={<SignIn />} />
+          <Route path='/' element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/' element={<Chatbot />} />
+            <Route path='/' element={<Prompt2Image />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/Pages/Prompt2Image.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Prompt2Image() {
+  return (
+    <div>Prompt2Image</div>
+  )
+}
