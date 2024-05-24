05bdd6091504010e8c6407bf50ec914f8d535bec
Samarth Narayan <kayaniv321@gmail.com>
2024-05-24 22:50:15 +0530

    express and mongodb setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+        .then(()=>{ console.log("Databse connected!!"); })
+        .catch((err)=>{ console.log(err) }); 
+
+const app=express();
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000!!');
+});
