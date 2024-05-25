4c42fc7965eca3c6e79d2c4af284da6e59869446
Samarth Narayan <kayaniv321@gmail.com>
2024-05-25 10:35:41 +0530

    redux setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,15 @@ import './index.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { store, persistor } from './redux/store.js'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <ToastContainer
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+      <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar={false}
@@ -20,5 +25,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         draggable
         pauseOnHover
         theme="colored" />
-  </React.StrictMode>,
+    </Provider>
+  </PersistGate>
 )
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,37 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState={
+    currentUser: null,
+    error: null,
+    loading: false
+}
+
+const userSlice=createSlice({
+    name:'user',
+    initialState,
+    reducers:{
+        signInStart:(state)=>{
+            state.loading=true;
+            state.error=null;
+        },
+        signInSuccess:(state,action)=>{
+            state.currentUser=action.payload;
+            state.loading=false;
+            state.error=null;
+        },
+        signInFailure:(state,action)=>{
+            state.loading=false;
+            state.error=action.payload;
+        },
+        signOutSuccess:(state,action)=>{
+            state.currentUser=null;
+            state.loading=false;
+            state.error=null;
+        },
+    }
+});
+
+export const {signInStart,signInSuccess,signInFailure,
+              signOutSuccess} = userSlice.actions;
+
+export default userSlice.reducer;
