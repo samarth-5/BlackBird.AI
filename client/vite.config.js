91659ab7137ed15a94c2b16dd9599f5147281f64
Samarth Narayan <kayaniv321@gmail.com>
2024-11-28 20:45:50 +0530

    adding env variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BACKEND_URL,
         secure: false,
       },
     },
