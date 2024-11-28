a7ffb8681810f22a5bf60dda49ee2e3223dddb97
Samarth Narayan <kayaniv321@gmail.com>
2024-11-28 21:04:31 +0530

    changes backend port

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log('Server is running on port 3000!!');
 });
 
