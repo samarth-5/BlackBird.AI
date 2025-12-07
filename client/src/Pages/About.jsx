8d2543d69d05d32f0e4d45572415ad27156a9c36
Samarth <kayaniv321@gmail.com>
2025-12-07 12:16:07 +0530

    fixed about section urls

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
       </div>
       <div className='flex justify-between'>
         <h1 className='text-3xl font-bold mb-4'>About <Link to='/'><span className='underline text-[#00ff31]'>BlackBird.AI</span></Link> (Developed by Samarth)</h1>
-        <Link target="_blank" to='https://samarth-dev.netlify.app/'><FaGlobe size={30} className='text-[#00ff31] mr-5' /></Link>
+        <Link target="_blank" to='https://samarth-fsdev.vercel.app/'><FaGlobe size={30} className='text-[#00ff31] mr-5' /></Link>
       </div>
       <p className='mb-4'>BlackBird.AI is your gateway to innovative AI solutions, offering an intuitive platform that combines advanced chatbot interactions and 
                           image generation capabilities. Powered by the Gemini API, our intelligent chatbot engages users in natural, context-aware conversations, 
@@ -26,7 +26,7 @@ export default function About() {
                           casual users, creatives, and professionals alike, offering a versatile platform that inspires and assists. Join us at BlackBird.AI and 
                           explore the future of AI-driven interaction and creativity.</p>
       <div className=' px-5 py-3 flex gap-3 outline outline-slate-600 hover:outline-[#00ff31] rounded-3xl w-min'>
-        <Link target="_blank" to='https://www.linkedin.com/in/samarth-narayan-4a4998250/' className='hover:scale-150 duration-500'><FaLinkedin size={25} className='hover:text-[#00ff31]' /></Link> 
+        <Link target="_blank" to='https://www.linkedin.com/in/samarth-narayan/' className='hover:scale-150 duration-500'><FaLinkedin size={25} className='hover:text-[#00ff31]' /></Link> 
         <Link target="_blank" to='https://github.com/samarth-5' className='hover:scale-150 duration-500'><FaGithub size={25} className='hover:text-[#00ff31]' /></Link>
         <Link target="_blank" to='https://leetcode.com/u/samarth_123_/' className='hover:scale-150 duration-500'><SiLeetcode size={25} className='hover:text-[#00ff31]' /></Link>
         <Link target="_blank" to='https://www.instagram.com/samarth123_/' className='hover:scale-150 duration-500'><FaInstagram size={25} className='hover:text-[#00ff31]' /></Link>
